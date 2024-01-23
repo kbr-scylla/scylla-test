e468f475112c7c1b6cbe728a84704e9ab066f8fb
Kamil Braun <kbraun@scylladb.com>
2024-01-23 15:36:33 +0100

    current_pane

--- lib/tmux.py
+++ lib/tmux.py
@@ -11,3 +11,12 @@ def current_session(serv: libtmux.Server) -> Optional[libtmux.Session]:
                 if p.get('pane_tty') == tty:
                     return sess
     return None
+
+def current_pane(serv: libtmux.Server) -> Optional[tuple[libtmux.Session, libtmux.Window, libtmux.Pane]]:
+    tty = os.ttyname(sys.stdout.fileno())
+    for sess in serv.list_sessions():
+        for w in sess.list_windows():
+            for p in w.list_panes():
+                if p.get('pane_tty') == tty:
+                    return sess, w, p
+    return None
